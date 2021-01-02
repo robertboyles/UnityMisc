81b11fccae8efb720badb1a1d9bf51946948aaf4
Robert Boyles <74901377+robertboyles@users.noreply.github.com>
2021-01-02 23:33:43 +0000

    Simple control scripts for testing follow cam
    Very simple control scripts for testing the follow cam classes.

---
+++ Simple Control/ControllerCamera.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Camera controller for a camera with the follow camera component.
+/// </summary>
+public class ControllerCamera : MonoBehaviour
+{
+    /// <summary>
+    /// Horizontal Rotation rate in rad/s
+    /// </summary>
+    [Range(0.01f, 20.0f)]
+    [SerializeField] private float rotationRateHorizontal = 3.5f;
+
+    /// <summary>
+    /// Vertical Translation rate
+    /// </summary>
+    [Range(0.01f, 50.0f)]
+    [SerializeField] private float translationRateVertical = 10.0f;
+
+    /// <summary>
+    /// Camera used to base inputs from.
+    /// </summary>
+    [SerializeField] private Camera activeCamera;
+
+    /// <summary>
+    /// Follow camera component from camera
+    /// </summary>
+    private FollowCamMovement cameraMotion;
+
+    /// <summary>
+    /// Initialise require components.
+    /// </summary>
+    void Start()
+    {
+        /// Attempt to extract and set the FollowCam component.
+        if (GetComponent<FollowCamMovement>() is FollowCamMovement movementComponent)
+        {
+            cameraMotion = movementComponent;
+        }
+        else
+        {
+            /// throw exception if this failed.
+            Debug.LogError("Failed to detect a FollowCamMovement component on camera game object.");
+            throw new System.ArgumentNullException("No FollowCamMovement found on camera.");
+        }
+    }
+
+    /// <summary>
+    /// Update camera with control inputs.
+    /// </summary>
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal2");
+        float y = Input.GetAxis("Vertical2");
+
+        cameraMotion.rotate(
+            x * Time.deltaTime * rotationRateHorizontal);
+
+        cameraMotion.stepVerticalOffset(
+            y * Time.deltaTime * translationRateVertical);
+    }
+}
---
+++ Simple Control/ControllerCharacterCamRelative.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+/// <summary>
+/// Player controller component that transforms the input to world coordinates from
+/// an active camera view frame.
+/// </summary>
+public class ControllerCharacterCamRelative : MonoBehaviour
+{
+    /// <summary>
+    /// Maximum translational speed in the x,z plane
+    /// </summary>
+    [Range(0.01f, 20.0f)]
+    [SerializeField] private float maxTranslationSpeed = 3.0f;
+
+    /// <summary>
+    /// Character controller extracted from the parent game object
+    /// </summary>
+    private CharacterController controller;
+
+    /// <summary>
+    /// Camera used to base inputs from.
+    /// </summary>
+    [SerializeField] private Camera activeCamera;
+
+    /// <summary>
+    /// Relative angle between view and world heading
+    /// </summary>
+    private float angle
+    {
+        get
+        {
+            if (activeCamera)
+            {
+                return Atan2(
+                    activeCamera.transform.forward.x, 
+                    activeCamera.transform.forward.z) * 180 / PI;
+            } 
+            else
+            {
+                return 0.0f;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initialise require components.
+    /// </summary>
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    /// <summary>
+    /// Update the character controller transform and rotation.
+    /// </summary>
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 velocityVector_CAM = new Vector3(x, 0.0f, z);
+        float inputScale = velocityVector_CAM.magnitude < 1.0f ? velocityVector_CAM.magnitude : 1.0f;
+        
+        Vector3 velocityVector_WORLD = transform_CAM2World_aboutY(
+            velocityVector_CAM,
+            angle);
+
+        controller.SimpleMove(
+            velocityVector_WORLD.normalized * maxTranslationSpeed * inputScale);
+
+        controller.transform.rotation = 
+            velocityVector_CAM.magnitude > 0.00001 ? Quaternion.LookRotation(velocityVector_WORLD) : 
+            controller.transform.rotation;
+    }
+
+
+    /// <summary>
+    /// Transform control input velocity vector in to world coordinates from camera view orientation.
+    /// </summary>
+    /// <param name="cam">Vector in camera view cooridinates</param>
+    /// <param name="angle">delta angle about the y axis between camera and world heading anlge</param>
+    /// <returns></returns>
+    private Vector3 transform_CAM2World_aboutY(Vector3 cam, float angle)
+    {
+        return Quaternion.AngleAxis(angle, Vector3.up) * cam;
+    }
+
+    /// <summary>
+    /// Set the active camera
+    /// </summary>
+    /// <param name="camera"></param>
+    public void setActiveCamera(Camera camera)
+    {
+        activeCamera = camera;
+    }
+}
